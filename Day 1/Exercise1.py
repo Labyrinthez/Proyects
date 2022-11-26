b9f9c1c383490819740b68c84b9d459bddab735b
Labyrinthez <119134896+Labyrinthez@users.noreply.github.com>
2022-11-26 14:32:55 -0600

    Add files via upload

---
+++ Day 1/Exercise1.py
@@ -0,0 +1,3 @@
+print("Day 1 - Print Python Function")
+print("The function is declared like this:")
+print("print('what to print')")
---
+++ Day 1/final_project.py
@@ -0,0 +1,4 @@
+print("Welcome to the band-name generator uwu\nIn this program I will show you your band's name, so let's get into it uwu")
+first_part = input("First of all tell me what is your city that you grew up in: ")
+second_part = input("What's your pet's name? ")
+print("So, your band's name is: {}".format(first_part + " " + second_part))
